9419b379db5e9ea691d57c9feb51d889fb8992d2
os3ketchup <eldorteshaboyev1994@gmail.com>
2025-06-20 23:24:31 +0500

    ds

---
+++ bin/e_florista_bot.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'package:dotenv/dotenv.dart';
+import 'package:e_florista_bot/bot.dart';
+
+
+Future<void> main() async {
+  String? token;
+
+  // ✅ Use .env only if running locally
+  if (Platform.environment.containsKey('RAILWAY_ENVIRONMENT')) {
+    token = Platform.environment['BOT_TOKEN'];
+  } else {
+    final env = DotEnv()..load();
+    token = env['BOT_TOKEN'];
+  }
+
+  if (token == null || token.isEmpty) {
+    print('❌ BOT_TOKEN not found.');
+    return;
+  }
+
+  await startBot(token);
+}
+///
